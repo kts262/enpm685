8dd4193ab86eef4369421430acc61556501b1768
kts262 <kevin.shivers@gmail.com>
2021-04-14 23:41:46 -0400

    Update exit-node4.py
    Updated for Python 3

--- exit-node4.py
+++ exit-node4.py
@@ -1,34 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import urllib2
+import requests
 from optparse import OptionParser
 
-parser = OptionParser(usage="Usage: %prog [-f format] file")
-parser.add_option("-f", dest="format", help="output format to use [csv, palo, iptables]")
+parser = OptionParser(usage="Usage %prog [-f format] file")
+parser.add_option("-f", dest="format", help="output format to use [csv ,palo, iptables]")
 
 options, args = parser.parse_args()
 
-if len(args) < 1:
-	parser.error("Wrong number of arguments")
+if len(args) < 2:
+    parser.error("You need to specify a format and a file name")
+    sys.exit(0)
 
-f = open(args[0], 'wb')
+f = open(args[0], "w")
 
-response = urllib2.urlopen('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1&port=80')
-exit_list = response.read()
+exit_list_url = requests.get('http://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1&port=80')
+
+exit_list = exit_list_url.text
 exit_list = exit_list.split("\n")
 exit_list.pop()  # delete that last blank line
+
 i = 0
-print "Pulling Tor exit node list..."
-print "Output option selected: " + options.format
+print("Pulling Tor exit node list...")
+print("Output option selected: " + options.format)
 
 for line in exit_list:
         if line[0] != "#":
-		if options.format == "csv":
-                	line_output = line + ",\n"
-		elif (options.format == "palo") or (options.format == "cidr"):
-			line_output = line + "/32\n"
-		f.write(line_output)
-                i = i+1
+                if options.format == "csv":
+                    f.write(line + ",\n")
+                i = i + 1
 
-print "Done. " + str(i) + " exit nodes"
-
+print("Done, " + str(i) + " exit nodes")
