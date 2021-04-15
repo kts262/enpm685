3202effdec96b4bdbe422a89304ad2448b914eef
kts262 <kevin.shivers@gmail.com>
2021-04-14 22:24:59 -0400

    Update wordpress-check2.py
    Updated for Python 3

--- wordpress-check2.py
+++ wordpress-check2.py
@@ -5,17 +5,14 @@ import requests
 
 
 def check_url(url):
-  try:
-    r = requests.get(url)
-    if r.status_code == 200:
-	print url + " - FOUND -- POSSIBLE WORDPRESS SITE"
-    else:
-        print url + " - NOT found"
-  except Exception, e:
-    pass
+  r = requests.get(url)
+  if r.status_code == 200:
+    print(url + " - FOUND -- POSSIBLE WORDPRESS SITE")
+  else:
+    print(url + " - NOT found")
 
 httpsvr = sys.argv[1]
 
-print "Checking " + httpsvr + "..."
+print("Checking " + httpsvr + "...")
 check_url(httpsvr + "/readme.html")
 check_url(httpsvr + "/wp-admin/admin-ajax.php")
