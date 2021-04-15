4a2391a2849bdbf2dac91992915e2710690064a2
kts262 <kevin.shivers@gmail.com>
2021-04-14 23:16:31 -0400

    Update exit-node.py
    Updated for Python3

--- exit-node.py
+++ exit-node.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import urllib2
+import requests
 
-response = urllib2.urlopen('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1&port=80')
-exit_list = response.read()
+exit_list_url = requests.get('http://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1&port=80')
+exit_list = exit_list_url.text
 exit_list = exit_list.split("\n")
 exit_list.pop()  # delete that last blank line
 
 for line in exit_list:
         if line[0] != "#":
-                print line + ","
-
+                print(line + ",")
